254a0363807fac466b7e96c686eba35ef74d36ee
wce <200070253+7wce@users.noreply.github.com>
2026-03-23 14:56:42 +0700

    Update Example.lua

--- Example.lua
+++ Example.lua
@@ -1,6 +1,6 @@
-local Module = require(Path.To.ArraylistModule)()
+local Module = require(script.Parent.ArraylistModule)()
 
-Module.title("Hello world!") -- Changes the title name.
+Module.title("Solstice Client | <font color=\"#c7a6f5\">v1.0.0</font>") -- Changes the title name.
 
 local Array1 = Module.new({
 	name = "Array" -- Will show text as: "Array"
@@ -8,7 +8,13 @@ local Array1 = Module.new({
 
 Module.updText({
 	array = Array1, -- The array provided, for this we'll use "Array1" because that's the array that we've just created.
-	newText = "Hello world!" -- Will change the text of the array
+	newText = "Hello <font color=\"#9146fa\">world!</font>" -- Will change the text of the array
 })
 
+task.wait(2)
+
 Module.Toggle(Array1) -- Will hide the array provided.
+
+task.wait(1)
+
+Module.Toggle(Array1) -- Will show the array provided.
